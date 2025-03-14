1d611c24ef1b355fb11a8f72b62fbc1fbda68927
BenjaminKvHa <benjamin.kvello.hansen@gmail.com>
2025-03-14 20:44:38 +0100

    feat: Implement dealHand method to pick n random cards

--- src/main/java/edu/ntnu/idi/idatt/DeckOfCards.java
+++ src/main/java/edu/ntnu/idi/idatt/DeckOfCards.java
@@ -2,11 +2,13 @@ package edu.ntnu.idi.idatt;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class DeckOfCards {
 
   private final List<PlayingCard> deck = new ArrayList<>();
   private final char[] suits = {'S', 'H', 'D', 'C'};
+  private final Random random = new Random();
 
   public DeckOfCards() {
     for (char suit : suits) {
@@ -14,6 +16,23 @@ public class DeckOfCards {
         deck.add(new PlayingCard(suit, face));
       }
     }
+  }
+
+  /**
+   * Picks n random cards from the deck (and removes them from the deck).
+   * @param n number of cards to pick
+   * @return a List of n randomly picked PlayingCards
+   */
+  public List<PlayingCard> dealHand(int n) {
+    if (n < 1 || n > deck.size()) {
+      throw new IllegalArgumentException("Cannot deal " + n + " cards. Deck has " + deck.size());
+    }
+    List<PlayingCard> hand = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      int index = random.nextInt(deck.size());
+      hand.add(deck.remove(index));
+    }
+    return hand;
   }
 
   public List<PlayingCard> getDeck() {
